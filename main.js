c466b2ff1999de3716d9ff467123b6c51810645c
simonequarticelli <quarti87@gmail.com>
2019-06-03 19:09:59 +0200

    inizio esercizio rest-todoolean

---
+++ main.js
@@ -0,0 +1,31 @@
+// creare una web app per gestire una todo-list.
+// Utilizzate l'API con il vostro link personalizzato
+// (v. messaggio precedente di Michele) e iniziate con la visualizzazione di tutti i todo,
+// l' *inserimento* di un nuovo todo e la *cancellazione* di un todo esistente.
+// Poi aggiungete la possibilità di *modificare* un todo esistente e,
+// come *bonus*, la possibilità di aggiungere un campo "orario" aggiuntivo
+
+// Simone: http://157.230.17.132:3015/todos
+
+
+
+$(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
